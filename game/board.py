cd10c508dcc8564015403baeab43a428d36d83c6
zaidbw <164448350+zaidbw@users.noreply.github.com>
2025-05-06 03:56:53 +0500

    Update board.py

--- game/board.py
+++ game/board.py
@@ -10,8 +10,8 @@ class Board:
 
     def check_win(self, marker):
         win_conditions = [
-            [0,1,2], [3,4,5], [6,7,8],  # Rows
-            [0,3,6], [1,4,7], [2,5,8],  # Columns
-            [0,4,8], [2,4,6]            # Diagonals
+            [0,1,2], [3,4,5], [6,7,8],
+            [0,3,6], [1,4,7], [2,5,8],  
+            [0,4,8], [2,4,6]            
         ]
         return any(all(self.board[i] == marker for i in combo) for combo in win_conditions)
